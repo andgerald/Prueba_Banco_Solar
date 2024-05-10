d829c0634f7003a078d6d2f43537bb4ce6113df7
andgeral <geraldinebecerra93@gmail.com>
2024-05-10 02:26:43 -0400

    se agregan comentarios

--- routes/usuariosRoute.js
+++ routes/usuariosRoute.js
@@ -4,7 +4,9 @@ import { Router } from "express";
 const router = Router();
 /// GET: Devuelve la aplicación cliente disponible en el apoyo de la prueba
 router.get("/", usuariosController.findAll);
+//usuario POST: Recibe los datos de un nuevo usuario y los almacena en PostgreSQL.
 router.post("/", usuariosController.create);
+//usuario DELETE: Recibe el id de un usuario registrado y lo elimina
 router.delete("/", usuariosController.remove);
 
 export default router;
