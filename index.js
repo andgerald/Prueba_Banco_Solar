4b06c6e629ce4c2100e77f8ee571cec1393128e1
andgeral <geraldinebecerra93@gmail.com>
2024-05-10 00:10:37 -0400

    se agrega ruta raiz con el html

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import "dotenv/config";
+import path from "path";
 
 const { SERVER_PORT } = process.env;
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening on port http://localhost:${SERVER_PORT}`);
