f114a710f4cae6f6be93eb049b359a723775179f
andgeral <geraldinebecerra93@gmail.com>
2024-05-10 01:37:33 -0400

    se agrega ruta base para usuarios

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import "dotenv/config";
 import path from "path";
+import usuariosRoute from "./routes/usuariosRoute.js";
 
 const { SERVER_PORT } = process.env;
 const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
+
+app.use("/usuario", usuariosRoute);
+app.use("/usuarios", usuariosRoute);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
