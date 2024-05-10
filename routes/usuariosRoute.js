9ca6bdb3262147869273973508e2bdec36785279
andgeral <geraldinebecerra93@gmail.com>
2024-05-10 02:24:34 -0400

    se agrega la ruta delete usuario

--- routes/usuariosRoute.js
+++ routes/usuariosRoute.js
@@ -5,5 +5,6 @@ const router = Router();
 /// GET: Devuelve la aplicación cliente disponible en el apoyo de la prueba
 router.get("/", usuariosController.findAll);
 router.post("/", usuariosController.create);
+router.delete("/", usuariosController.remove);
 
 export default router;
