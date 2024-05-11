79355d23a6b4a16e3cebb7736a68a6e3e93ce148
andgeral <geraldinebecerra93@gmail.com>
2024-05-10 21:54:17 -0400

    se agrega la ruta de usuarios correctamente

--- routes/usuariosRoute.js
+++ routes/usuariosRoute.js
@@ -3,11 +3,11 @@ import { Router } from "express";
 
 const router = Router();
 /// GET: Devuelve la aplicación cliente disponible en el apoyo de la prueba
-router.get("/", usuariosController.findAll);
+router.get("/usuarios", usuariosController.findAll);
 //usuario POST: Recibe los datos de un nuevo usuario y los almacena en PostgreSQL.
-router.post("/", usuariosController.create);
+router.post("/usuario", usuariosController.create);
 //usuario DELETE: Recibe el id de un usuario registrado y lo elimina
-router.delete("/", usuariosController.remove);
+router.delete("/usuario", usuariosController.remove);
 //usuario PUT: Recibe los datos modificados de un usuario registrado y los actualiza
-router.put("/", usuariosController.update);
+router.put("/usuario", usuariosController.update);
 export default router;
