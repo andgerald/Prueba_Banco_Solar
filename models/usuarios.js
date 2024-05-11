1c38b1fd54ceca306e31cb4725e68b319125cfa2
andgeral <geraldinebecerra93@gmail.com>
2024-05-11 00:08:31 -0400

    se agregan errores para los usuarios

--- models/usuarios.js
+++ models/usuarios.js
@@ -3,41 +3,26 @@ import pool from "../database/dbConfig.js";
 
 //usuarios GET: Devuelve todos los usuarios registrados con sus balances.
 const findAll = async () => {
-  let message;
   const result = await pool.query("SELECT * FROM usuarios");
-  rows.length == 0
-    ? (mensaje = "No hay registros en la tabla")
-    : (mensaje = "Lista de registros encontrados");
-  return {
-    result,
-    message: message,
-  };
+  return result.rows;
 };
 
 //usuario POST: Recibe los datos de un nuevo usuario y los almacena en PostgreSQL.
 const create = async (usuario) => {
-  try {
-    const result = await pool.query(
-      "INSERT INTO usuarios (nombre, balance) values($1,$2) RETURNING *",
-      [usuario.nombre, usuario.balance]
-    );
-    return result.rows[0];
-  } catch (e) {
-    return e;
-  }
+  const result = await pool.query(
+    "INSERT INTO usuarios (nombre, balance) values($1,$2) RETURNING *",
+    [usuario.nombre, usuario.balance]
+  );
+  return result.rows[0];
 };
 
 //usuario DELETE: Recibe el id de un usuario registrado y lo elimina
 const remove = async (id) => {
-  try {
-    const result = await pool.query(
-      "DELETE FROM usuarios WHERE id=$1 RETURNING *",
-      [id]
-    );
-    return result.rows[0];
-  } catch (e) {
-    return e;
-  }
+  const result = await pool.query(
+    "DELETE FROM usuarios WHERE id=$1 RETURNING *",
+    [id]
+  );
+  return result.rows[0];
 };
 
 const update = async (nombre, balance, id) => {
