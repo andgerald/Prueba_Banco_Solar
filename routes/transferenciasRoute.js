8d90a1a1b4b1812bdc13f93f7847ce801c780ffd
andgeral <geraldinebecerra93@gmail.com>
2024-05-10 21:54:05 -0400

    se  agregan la ruta de transferencias correctamente

--- routes/transferenciasRoute.js
+++ routes/transferenciasRoute.js
@@ -4,7 +4,7 @@ import { Router } from "express";
 const router = Router();
 
 //transferencias GET: Devuelve todas las transferencias almacenadas en la base de datos en formato de arreglo.
-router.get("/", transferenciasController.findAll);
-router.post("/", transferenciasController.create);
+router.get("/transferencias", transferenciasController.findAll);
+router.post("/transferencia", transferenciasController.create);
 
 export default router;
